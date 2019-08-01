16edea6d39b18090adfb026e608758c8de8466a2
David Lara <42874269+apthos@users.noreply.github.com>
2019-07-31 23:21:35 -0400

    Add stream command

---
+++ OWL/commands/stream.js
@@ -0,0 +1,41 @@
+const Discord = require('discord.js');
+const request = require('request');
+
+module.exports = {
+	name: 'stream',
+	description: 'Overwatch League Information!',
+	usage: '!owl stream',
+	execute(message, args) {
+		const url = "https://api.overwatchleague.com/v2/streams";
+
+		//Finding team
+		request(url, function(error, response, body){
+			if (!error) {
+				try {
+                    let info = JSON.parse(body);
+                    let stream = info.attributes.stream;
+
+                    //Creates and sends embedded message with stream info		
+			        let embedStream = new Discord.RichEmbed()
+				        .setColor('#6441a5')
+                        .setTitle(stream.stream_type.toUpperCase())
+                        .setURL(stream.channel.url)
+				        .addField('Overwatch League', stream.channel.status)
+                        .setThumbnail(stream.channel.logo);
+			        message.channel.send(embedStream);
+                    
+				} 
+				catch(err) {
+					message.reply(err);
+
+				}		
+
+			}
+			
+		})
+
+
+
+	}
+
+};
